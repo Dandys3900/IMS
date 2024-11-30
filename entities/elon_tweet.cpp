f026d5b9abd16048c2b8f78fdd47f5f3f99b7cba
Dandys <tomasdaniel7800@gmail.com>
2024-11-30 21:35:00 +0100

    Add simulation statistics for Elon class.

--- entities/elon_tweet.cpp
+++ entities/elon_tweet.cpp
@@ -2,23 +2,33 @@
 #include "elon_tweet.h"
 
 ElonTweet::ElonTweet(Coin* coin)
-    : coin(coin)
+    : coin(coin),
+      done_changes("")
 {
 }
 
 ElonTweet::~ElonTweet() {
-
+    this->PrintStats();
 }
 
 void ElonTweet::Behavior() {
     while (true) {
-        // Randomly select factor of price change (according to study between <-11.8% , +16.9%>)
+        // Repeat every 2-8 month
+        Wait(Uniform(2 * EACH_MONTH, 8 * EACH_MONTH));
+
+        // Randomly select factor of price change (according to study between <-11.8% , +16.9%))
         double change_factor = Uniform(-0.118, 0.169);
 
         // Change coin's price
         coin->ChangePrice(change_factor);
-
-        // Repeat every 2-8 month
-        Wait(Uniform(2 * EACH_MONTH, 8 * EACH_MONTH));
+        // Store record about this change
+        done_changes += "Change price about " + to_string(change_factor * 100) + " %" + "\n";
     }
+}
+
+void ElonTweet::PrintStats() {
+    cout << "-------------------------" << endl;
+    cout << "Unpredicted price change stats (aka Elon tweet):" << endl;
+    cout << this->done_changes << endl;
+    cout << "-------------------------" << endl;
 }
--- entities/elon_tweet.h
+++ entities/elon_tweet.h
@@ -8,12 +8,14 @@ class Coin;
 class ElonTweet : public Process {
     private:
         Coin* coin;
+        string done_changes; // Document all price changes done by this class
 
     public:
         ElonTweet(Coin* coin);
         ~ElonTweet();
 
         void Behavior() override;
+        void PrintStats();
 };
 
 #endif // ELONTWEET_H

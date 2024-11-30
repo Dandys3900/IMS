f1b7913583de9c24ec48f5c2f09e0cb059973c18
Jakub Janšta <kubulambula@gmail.com>
2024-11-30 17:38:22 +0100

    merge main

--- entities/crypto_investor.cpp
+++ entities/crypto_investor.cpp
@@ -79,86 +79,11 @@ void Investor::PrintStats() {
     cout << "-------------------------" << endl;
     cout << "Investor stats:"           << endl;
     cout << " -> Traided coins: "       << endl;
-    for (auto coin : this->coins) {
-        string coinname = coin->GetCoinName();
-        cout << "Name: " << coinname << " final balance: " << this->balance.at(coinname) << endl;
+    for (auto coin : this->balance) {
+        string coinname = coin.first;
+        cout << "Name: " << coinname << " final balance: " << coin.second << endl;
         cout << "Sell threshold: " << this->thresholds.at(coinname).first << endl;
         cout << "Buy threshold: " << this->thresholds.at(coinname).second << endl;
     }
     cout << "-------------------------" << endl;
 }
-
-
-// void LongTermInvestor::Behavior() {
-//     while (true) {
-//         for (Coin* coin : this->coins) {
-//             if (coin->GetCurrentPrice() < this->thresholds.at(coin->GetCoinName()).first) {
-//                 this->SellCoins(coin);
-//             } else if (coin->GetCurrentPrice() > this->thresholds.at(coin->GetCoinName()).second) {
-//                 this->BuyCoins(coin);
-//             }
-//         }
-
-//         Wait(EVERY_WEEK);
-//     }
-// }
-
-// void LongTermInvestor::BuyCoins(Coin* coin) {
-//     Exchange* exchange = Exchange::SelectRandomExchangeFor(coin);
-//     if (coin == nullptr) {
-//         return;
-//     }
-//     double coins_to_buy = Normal((84 / coin->GetCurrentPrice()), 10);
-//     coins_to_buy += coins_to_buy * this->GetInvestorSentiment() * 0.5; // not as affected by his sentiment
-//     double coins_bought = exchange->ExecuteTransaction(coins_to_buy, this, coin, TransactionType::BUY);
-//     this->balance.at(coin->GetCoinName()) += coins_bought;
-// }
-
-// void LongTermInvestor::SellCoins(Coin* coin) {
-//     Exchange* exchange = Exchange::SelectBestExchangeFor(coin);
-//     if (coin == nullptr) {
-//         return;
-//     }
-//     // Sell 75% of coins, but at least one whole coin
-//     double coins_to_sell = this->balance.at(coin->GetCoinName()) > 1.0 ? this->balance.at(coin->GetCoinName()) * 0.75 : 1.0;
-//     coins_to_sell -= coins_to_sell * this->GetInvestorSentiment() * 0.25; // not as affected by his sentiment
-//     double coins_sold = exchange->ExecuteTransaction(coins_to_sell, this, coin, TransactionType::SELL);
-//     this->balance.at(coin->GetCoinName()) -= coins_sold;
-// }
-
-// void ShortTermInvestor::Behavior() {
-//     while (true) {
-//         for (Coin* coin : this->coins) {
-//             if (coin->GetCurrentPrice() < this->thresholds.at(coin->GetCoinName()).first) {
-//                 this->SellCoins(coin);
-//             } else if (coin->GetCurrentPrice() > this->thresholds.at(coin->GetCoinName()).second) {
-//                 this->BuyCoins(coin);
-//             }
-//         }
-
-//         Wait(EVERY_12H);
-//     }
-// }
-
-// void ShortTermInvestor::BuyCoins(Coin* coin) {
-//     Exchange* exchange = Exchange::SelectRandomExchangeFor(coin);
-//     if (coin == nullptr) {
-//         return;
-//     }
-//     double coins_to_buy = Normal((126 / coin->GetCurrentPrice()), 10);;
-//     coins_to_buy += coins_to_buy * this->GetInvestorSentiment();
-//     double coins_bought = exchange->ExecuteTransaction(coins_to_buy, this, coin, TransactionType::BUY);
-//     this->balance.at(coin->GetCoinName()) += coins_bought;
-// }
-
-// void ShortTermInvestor::SellCoins(Coin* coin) {
-//     Exchange* exchange = Exchange::SelectBestExchangeFor(coin);
-//     if (coin == nullptr) {
-//         return;
-//     }
-//     // Sell 75% of coins, but at least one whole coin
-//     double coins_to_sell = this->balance.at(coin->GetCoinName()) > 1.0 ? this->balance.at(coin->GetCoinName()) * 0.75 : 1.0;
-//     coins_to_sell -= coins_to_sell * this->GetInvestorSentiment();
-//     double coins_sold = exchange->ExecuteTransaction(coins_to_sell, this, coin, TransactionType::SELL);
-//     this->balance.at(coin->GetCoinName()) -= coins_sold;
-// }
